1b7fcb9497795ca2a09a48cce3dafb4467115356
siraphop1995 <siraphop.a95@gmail.com>
2019-10-03 16:34:39 +0700

    refactor server.js

--- server.js
+++ server.js
@@ -5,35 +5,31 @@ const cors = require('cors');
 const process = require('process');
 const errorHandler = require('./src/utils/errorHandler');
 
-//Readind .env file
-const dotenv = require('dotenv');
-dotenv.config();
-
 app = express();
 port = process.env.PORT || 3000;
-
-User = require('./src/models/userListModel');
 
 mongoose.Promise = require('bluebird');
 const mongooseConfig = {
   useNewUrlParser: true,
   useCreateIndex: true
 };
-mongoose.set('useCreateIndex', true);
 mongoose.connect(process.env.MONGO_URL, mongooseConfig, error => {
   if (error) throw error;
   console.log('Successfully connected');
 });
 
+//Express middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(errorHandler);
 
+//Model and router
+User = require('./src/models/userListModel');
 const userRouter = require('./src/routes/userRouter');
 app.use(userRouter);
 
-app.use(errorHandler);
-
+//Listen port
 app.listen(port, () => {
   console.log('Start listen on port: ' + port);
 });
